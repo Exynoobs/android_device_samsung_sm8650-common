45fa04ed7d43cbc09d3b5131229c1fe138b93be8
Alexander Martinz <amartinz@shiftphones.com>
2025-02-06 13:48:34 +0100

    sm8650-common: enable e-SIM support
    Change-Id: Ib111d397ae87ded93a228673988f5f7f6ab5a5cd
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>
    Co-authored-by: Antoine Colombier <hi@acolombier.dev>
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- common.mk
+++ common.mk
@@ -125,6 +125,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+# e-SIM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/default-permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/default-permissions_com.google.android.euicc.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp_allowlist_com.google.android.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp_allowlist_com.google.android.euicc.xml \
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
+
 # Fastboot
 PRODUCT_PACKAGES += \
     fastbootd
