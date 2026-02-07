002bc7decc55169443bb50fa1db26f9db954c07c
David Wronek <david.wronek@mainlining.org>
2026-02-08 00:24:16 +0100

    sm8650-common: Update proprietary files from Samsung S928BXXS4CYK2

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.file import File
 from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
@@ -25,7 +26,6 @@ namespace_imports = [
     'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/commonsys/display',
     'vendor/qcom/opensource/dataservices',
-    'vendor/qcom/opensource/display',
 ]
 
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
@@ -41,36 +41,37 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    ('vendor/bin/hw/android.hardware.security.keymint-service-qti', 'vendor/lib64/libskeymint10device.so', 'vendor/lib64/libskeymint_cli.so'): blob_fixup()
-        .add_needed('android.hardware.security.rkp-V3-ndk.so')
-        .replace_needed('libcrypto.so', 'libcrypto-v33.so')
-        .replace_needed('libcppbor_external.so', 'libcppbor.so'),
-    'vendor/lib64/hw/gatekeeper.mdfpp.so': blob_fixup()
+    'vendor/etc/init/android.hardware.security.keymint-service-qti.rc': blob_fixup()
+        .regex_replace('android.hardware.security.keymint-service', 'android.hardware.security.keymint-service-qti'),
+    ('vendor/bin/hw/android.hardware.security.keymint-service-spu-qti', 'vendor/lib64/libspukeymint.so'): blob_fixup()
+        .replace_needed('android.hardware.security.sharedsecret-V2-ndk.so', 'android.hardware.security.sharedsecret-V1-ndk.so'),
+    'vendor/lib64/libskeymint_cli.so': blob_fixup()
         .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
-    ('vendor/lib64/ese_spi_nxp.so', 'vendor/lib64/nfc_nci_nxpsn.so'): blob_fixup()
-	.add_needed('libbase_shim.so'),
     'vendor/lib64/libsec-ril.so': blob_fixup()
         .binary_regex_replace(b'ril.dds.call.ongoing', b'vendor.calls.slot_id')
         .sig_replace('60 0E 40 F9 E1 03 15 AA 82 0C 80 52 E3 03 14 AA', '60 0E 40 F9 E1 03 15 AA 82 0C 80 52 30 08 0D D2'),
-    'vendor/etc/vintf/manifest/sec_c2_manifest_default0_1_0.xml': blob_fixup()
+    'vendor/etc/vintf/manifest/sec_c2_manifest_default0_1_2.xml': blob_fixup()
         .regex_replace('default0', 'software'),
-    'vendor/etc/init/vendor.qti.media.c2audio@1.0-service.rc': blob_fixup()
-        .regex_replace('.*disabled.*\n', ''),
     ('vendor/etc/media_codecs_pineapple.xml'): blob_fixup()
         .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('libcodec2_shim.so'),
-    'vendor/lib64/unihal_android.so': blob_fixup()
+    ('vendor/lib64/unihal_android.so', 'vendor/lib64/libc2filterplugin.so'): blob_fixup()
         .add_needed('libui_shim.so'),
     'vendor/lib64/libsamsungcamerahal.so': blob_fixup()
         .sig_replace('E0 8A', '94 8B'),
-    'vendor/etc/init/android.hardware.security.keymint-service-qti.rc': blob_fixup()
-        .regex_replace('android.hardware.security.keymint-service', 'android.hardware.security.keymint-service-qti'),
     (
         'vendor/lib64/libdpps.so',
+        'vendor/lib64/liblearningmodule.so',
         'vendor/lib64/libsnapdragoncolor-manager.so',
     ): blob_fixup()
-        .replace_needed('libtinyxml2.so', 'libtinyxml2_1.so'),
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    (
+        'vendor/lib64/libVoiceSdk.so',
+        'vendor/lib64/libcapiv2uvvendor.so',
+        'vendor/lib64/liblistensoundmodel2vendor.so',
+    ): blob_fixup()
+        .replace_needed('libtensorflowlite_c.so', 'libtensorflowlite_c_vendor.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
